816488ec6483765f6c2e33e6d0fe89b8b916af19
Youssef Taleb <yt621671@gr10-07.esirem-ad.ad.u-bourgogne.fr>
2021-01-11 12:17:14 +0100

    [REMAKE] views set

---
+++ taskss_planner/taskss_planner/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskss_planner
+//
+//  Created by Youssef Taleb on 11/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
